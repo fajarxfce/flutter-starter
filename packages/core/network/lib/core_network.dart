4cfe544ab9b8086f364dd2ef4c54d820354c8b1a
fajarxfce <fajaralamsyah000@gmail.com>
2026-09-23 22:32:34 +0700

    feat(network): add a network resource persistence boundary

--- packages/core/network/lib/core_network.dart
+++ packages/core/network/lib/core_network.dart
@@ -4,3 +4,4 @@ export 'src/di/network_clients.dart';
 export 'src/interceptors/credential_interceptor.dart';
 export 'src/interceptors/safe_logging_interceptor.dart';
 export 'src/mappers/network_failure_mapper.dart';
+export 'src/resources/network_bound_resource.dart';
